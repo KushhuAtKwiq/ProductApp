02bddcea773ebc198c4c62f8dd0b11837d233751
Kushagra Gangwal <kushagragangwal@gmail.com>
2024-01-18 15:58:28 +0530

    update

--- src/app/Product/ProductList/list-product/list-product.component.ts
+++ src/app/Product/ProductList/list-product/list-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Product } from '../../../model/Product';
+import { ProductService } from '../../../Service/product.service';
 
 /**
  * iterates through input array
@@ -13,7 +14,16 @@ import { Product } from '../../../model/Product';
   imports: [RouterModule],
   templateUrl: './list-product.component.html',
   styleUrl: './list-product.component.css',
+  providers: [ProductService],
 })
 export class ListProductComponent {
   @Input() product: Product;
+
+  constructor(private ProductService: ProductService) {}
+
+  add() {
+    // this.ProductService.addToCart = this.product;
+    // console.log(this.ProductService.getCart);
+    console.log('hellop');
+  }
 }
